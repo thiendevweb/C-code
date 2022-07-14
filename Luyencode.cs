3c4c6d46b11a4aa35139aae65f6cf7daab3d97ab
Tran_Dinh_Thien <thiendinhtranbk65@gmail.com>
2022-07-14 19:11:29 +0700

    Number Learning

---
+++ Luyencode.cs
@@ -0,0 +1,24 @@
+// using System;
+
+// namespace Luyencode{
+//     class Learning{
+//         static void Main(string[] args){
+//             System.Console.Write("Nhap so can in: ");
+//             int number = Convert.ToInt32(Console.ReadLine());
+//             System.Console.Write("Nhap kich thuoc in ra: ");
+//             int size = Convert.ToInt32(Console.ReadLine());
+//             // for (int i = size - 1; i >= 0; i--)
+//             // {
+//             //     for (int j = 0; j <= i; j++)
+//             //         System.Console.Write(number + " ");
+//             //     System.Console.WriteLine();
+//             // }
+//             for (int i = 0; i < size; i++)
+//             {
+//                 for (int j = 0; j < size - i; j++)
+//                     System.Console.Write(number + " ");
+//                 System.Console.WriteLine();
+//             }
+//         }
+//     }
+// }
