a1db64abb266549af8fe806b71064f38009f22bb
thiendevweb <86286523+thiendevweb@users.noreply.github.com>
2022-07-14 21:04:19 +0700

    Update filemoi.cs

--- filemoi.cs
+++ filemoi.cs
@@ -3,7 +3,8 @@ using System;
 namespace newtest{
     class newmoi{
         static void Main(string[] args){
-            System.Console.WriteLine("hwllo");
+            System.Console.WriteLine("Hello World !!!!");
+            System.Console.Writeline("Nhap kich thuoc mang: ");
         }
     }
 }
